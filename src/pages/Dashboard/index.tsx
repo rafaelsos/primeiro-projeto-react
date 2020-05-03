8b36d1e4c4d44397a836afc9ddd51404069d92b5
Sosnowski0304 <rafael.sosnowski@gmail.com>
2020-05-03 02:10:05 -0300

    styling page dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="test">
+          <img
+            src="https://avatars2.githubusercontent.com/u/59904413?s=460&u=d0f144e2a300fcedabb86b23854e58e36a3e3df8&v=4"
+            alt="Rafael Sosnowski"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
